fc80665d9dbf17b318a9be1110e746786037f0ba
Noob <akhilkrishnaus1234@gmail.com>
2025-11-11 20:38:54 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -2,23 +2,11 @@ from aiohttp import web
 
 routes = web.RouteTableDef()
 
-#Dont Remove My Credit @Silicon_Bot_Update 
-#This Repo Is By @Silicon_Official 
-# For Any Kind Of Error Ask Us In Support Group @Silicon_Botz 
-
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("Silicon")
-
-#Dont Remove My Credit @Silicon_Bot_Update 
-#This Repo Is By @Silicon_Official 
-# For Any Kind Of Error Ask Us In Support Group @Silicon_Botz 
+    return web.json_response("I am Alive...!!!")
 
 async def web_server():
     web_app = web.Application(client_max_size=30000000)
     web_app.add_routes(routes)
     return web_app
-    
- #Dont Remove My Credit @Silicon_Bot_Update 
-#This Repo Is By @Silicon_Official 
-# For Any Kind Of Error Ask Us In Support Group @Silicon_Botz    
